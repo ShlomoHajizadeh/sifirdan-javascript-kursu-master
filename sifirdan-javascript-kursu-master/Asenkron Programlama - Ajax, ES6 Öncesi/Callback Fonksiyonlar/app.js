9c46bedca7b160f4e477c9bbd02b5008a7db2544
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-04-23 14:55:53 +0400

    Changed learn

--- sifirdan-javascript-kursu-master/Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
+++ sifirdan-javascript-kursu-master/Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
@@ -1,52 +1,50 @@
 // Callback Fonksiyonlar
 
-// 
+// const langs = ["C#", "Ruby", "Kotlin"]; 
 
-// langs.forEach(function(lang){
+// langs.forEach(function (lang) {
 //     console.log(lang);
 // });
 
-// document.getElementById("btn").addEventListener("click",function(){
-//     console.log("Tıkla");
+document.getElementById("btn").addEventListener("click", function () {
+    console.log("Tıkla");
+});
 
-// });
-// function process1(callback){
-//     setTimeout(function(){
-//         console.log("Process 1");
-//         callback();
+function process1(callback) {
+    setTimeout(function () {
+        console.log("Process 1");
+        callback();
+    }, 3000);
+}
 
-//     },3000);
-// }
-// function process2(){
-//     setTimeout(function(){
-//         console.log("Process 2");
-//     },2000);
-    
-// }
+function process2() {
+    setTimeout(function () {
+        console.log("Process 2");
+    }, 2000);
+}
 
-// process1(process2);
+process1(process2);
+process2()
+console.log("Naber");
 
-const langs = ["Python","Java","C++"];
+const langs = ["Python", "Java", "C++"];
 
-function addLang(lang,callback){
+function addLang(lang, callback) {
 
-    setTimeout(function(){
+    setTimeout(function () {
         langs.push(lang);
         console.log("Eklendi");
         callback();
-    },2000);
+    }, 2000);
 }
-function getAllLangs(){
-    setTimeout(function(){
-        langs.forEach(function(lang){
+
+function getAllLangs() {
+    setTimeout(function () {
+        langs.forEach(function (lang) {
             console.log(lang);
         });
-    },1000);
+    }, 1000);
 }
 
-addLang("Javascript",getAllLangs);
-
-
-
-
-
+addLang("Javascript", getAllLangs);
+getAllLangs(); // Asenkron Durum
