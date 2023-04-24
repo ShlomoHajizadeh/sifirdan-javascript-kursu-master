ae6caf3306affcf32a2b06b741a6ce62b254816b
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-04-24 15:48:05 +0400

    changed learn

--- sifirdan-javascript-kursu-master/Ajax - HTTP Requests/app.js
+++ sifirdan-javascript-kursu-master/Ajax - HTTP Requests/app.js
@@ -10,7 +10,7 @@ class Request {
     get(url, callback) {
 
         this.xhr.open("GET", url); // Bağlantı açık
-
+        
         this.xhr.onload = () => {
 
             if (this.xhr.status === 200) {
@@ -20,9 +20,10 @@ class Request {
                 // Hata durumuna 
                 callback("Get Request: Bir hata oluştu", null);
             }
-        };
+        }
         this.xhr.send();
     }
+
     post(url, data, callback) {
         this.xhr.open("POST", url);
         this.xhr.setRequestHeader("Content-type", "application/json"); // JSON Verisi
@@ -38,6 +39,7 @@ class Request {
         }
         this.xhr.send(JSON.stringify(data));
     }
+
     put(url, data, callback) {
         this.xhr.open("PUT", url);
         this.xhr.setRequestHeader("Content-type", "application/json"); // JSON Verisi
@@ -53,6 +55,7 @@ class Request {
         }
         this.xhr.send(JSON.stringify(data));
     }
+
     delete(url, callback) {
 
         this.xhr.open("DELETE", url); // Bağlantı açık
@@ -71,59 +74,64 @@ class Request {
         this.xhr.send();
     }
 }
+
 const request = new Request();
 
-// request.get("https://jsonplaceholder.typicode.com/albums", function (err, response) {
-//     if (err === null) {
-//         // Başarılı
-//         console.log(response);
-//     }
-//     else {
-//         // Hata
-//         console.log(err);
-//     }
-// });
+const albums  = request.get("https://jsonplaceholder.typicode.com/albums");
 
-// request.get("https://jsonplaceholder.typicode.com/albums/51", function (err, response) {
-//     if (err === null) {
-//         // Başarılı
-//         console.log(response);
-//     }
-//     else {
-//         // Hata
-//         console.log(err);
-//     }
-// });
+console.log(albums)
 
-// request.post("https://jsonplaceholder.typicode.com/albums", { userId: 2, title: "Thriller" }, function (err, album) {
+request.get("https://jsonplaceholder.typicode.com/albums", function (err, response) {
+    if (err === null) {
+        // Başarılı
+        console.log(response);
+    }
+    else {
+        // Hata
+        console.log(err);
+    }
+});
 
-//     if (err === null) {
-//         console.log(album);
-//     }
-//     else {
-//         // Hata
-//         console.log(err);
-//     }
-// })
+request.get("https://jsonplaceholder.typicode.com/albums/51", function (err, response) {
+    if (err === null) {
+        // Başarılı
+        console.log(response);
+    }
+    else {
+        // Hata
+        console.log(err);
+    }
+});
 
-// request.put("https://jsonplaceholder.typicode.com/albums/10", { userId: 143, title: "Tarkan Karma" }, function (err, album) {
+request.post("https://jsonplaceholder.typicode.com/albums", { userId: 2, title: "Thriller" }, function (err, album) {
 
-//     if (err === null) {
-//         console.log(album);
-//     }
-//     else {
-//         // Hata
-//         console.log(err);
-//     }
-// })
+    if (err === null) {
+        console.log(album);
+    }
+    else {
+        // Hata
+        console.log(err);
+    }
+})
 
-// request.delete("https://jsonplaceholder.typicode.com/albums/10", function (err, response) {
-//     if (err === null) {
-//         // Başarılı
-//         console.log(response);
-//     }
-//     else {
-//         // Hata
-//         console.log(err);
-//     }
-// });
+request.put("https://jsonplaceholder.typicode.com/albums/10", { userId: 143, title: "Tarkan Karma" }, function (err, album) {
+
+    if (err === null) {
+        console.log(album);
+    }
+    else {
+        // Hata
+        console.log(err);
+    }
+})
+
+request.delete("https://jsonplaceholder.typicode.com/albums/10", function (err, response) {
+    if (err === null) {
+        // Başarılı
+        console.log(response);
+    }
+    else {
+        // Hata
+        console.log(err);
+    }
+});
