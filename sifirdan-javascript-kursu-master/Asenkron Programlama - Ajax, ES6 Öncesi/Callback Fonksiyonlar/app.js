128d3e669a4b8cf23d05fbdc984eae188ccc9c75
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-05-14 18:16:16 +0400

    changed and learned

--- sifirdan-javascript-kursu-master/Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
+++ sifirdan-javascript-kursu-master/Asenkron Programlama - Ajax, ES6 Öncesi/Callback Fonksiyonlar/app.js
@@ -6,26 +6,27 @@
 //     console.log(lang);
 // });
 
-document.getElementById("btn").addEventListener("click", function () {
-    console.log("Tıkla");
-});
+// document.getElementById("btn").addEventListener("click", function () {
+//     console.log("Tıkla");
+// });
 
-function process1(callback) {
-    setTimeout(function () {
-        console.log("Process 1");
-        callback();
-    }, 3000);
-}
+// Asenkron İşlemler
+// function process1(callback) {
+//     setTimeout(function () {
+//         console.log("Process 1");
+//         callback();
+//     }, 3000);
+// }
 
-function process2() {
-    setTimeout(function () {
-        console.log("Process 2");
-    }, 2000);
-}
+// function process2() {
+//     setTimeout(function () {
+//         console.log("Process 2");
+//     }, 2000);
+// }
 
-process1(process2);
-process2()
-console.log("Naber");
+// process1(process2);
+// process2()
+// console.log("Naber");
 
 const langs = ["Python", "Java", "C++"];
 
