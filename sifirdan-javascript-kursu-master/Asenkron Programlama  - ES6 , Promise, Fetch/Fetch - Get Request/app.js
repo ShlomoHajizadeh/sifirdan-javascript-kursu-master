521fe07cfebaf99665276402d3436367c752c2cd
software1engineer1Solomon <suleyman.hacizade1@gmail.com>
2023-05-31 13:32:22 +0400

    changed and learned

--- sifirdan-javascript-kursu-master/Asenkron Programlama  - ES6 , Promise, Fetch/Fetch - Get Request/app.js
+++ sifirdan-javascript-kursu-master/Asenkron Programlama  - ES6 , Promise, Fetch/Fetch - Get Request/app.js
@@ -6,11 +6,11 @@ class Request {
             .then(response => response.json())
             .then(data => resolve(data))
             .catch(err => reject(err));
-
         })
 
         
     }
+
     post(url,data){
 
         return new Promise((resolve,reject) => {
@@ -24,11 +24,7 @@ class Request {
             .then(response => response.json())
             .then(data => resolve(data))
             .catch(err => reject(err));
-
-
         })
-        
-
     }
 
     put(url,data){
@@ -39,17 +35,13 @@ class Request {
                 headers: {
                 "Content-type": "application/json; charset=UTF-8"
                 }
-    
             })
             .then(response => response.json())
             .then(data => resolve(data))
             .catch(err => reject(err));
-
-
-
         })
-
     }
+
     delete(url){
 
         return new Promise((resolve,reject) => {
@@ -57,39 +49,26 @@ class Request {
             method: 'DELETE'
             }).then(response => resolve("Veri silme işlemi başarılı"))
             .catch(err => reject(err));
-
-
         });
     }
-
 }
 const request = new Request();
-
-// request.get("https://jsonplaceholder.typicode.com/albums")
-// .then(albums => {
+let albums;
+request.get("https://jsonplaceholder.typicode.com/albums")
+.then(albums => {
     
-//     console.log(albums);
-// })
-// .catch(err => console.log(err));
+    console.log(albums);
+})
+.catch(err => console.log(err));
 
-// request.post("https://jsonplaceholder.typicode.com/albums",{userId:1,title:"Thriller"})
-// .then(newAlbum => console.log(newAlbum))
-// .catch(err => console.log(err));
+request.post("https://jsonplaceholder.typicode.com/albums",{userId:1,title:"Thriller"})
+.then(newAlbum => console.log(newAlbum))
+.catch(err => console.log(err));
 
+request.put("https://jsonplaceholder.typicode.com/albums/10",{userId:10,title:"Tarkan Karma"})
+.then(album => console.log(album))
+.catch(err => console.log(err));
 
-// request.put("https://jsonplaceholder.typicode.com/albums/10",{userId:10,title:"Tarkan Karma"})
-// .then(album => console.log(album))
-// .catch(err => console.log(err));
-
-
-
-// request.delete("https://jsonplaceholder.typicode.com/albums/1")
-// .then(message => console.log(message) )
-// .catch(err => console.log(err));
-
-
-
-
-
-
-
+request.delete("https://jsonplaceholder.typicode.com/albums/1")
+.then(message => console.log(message) )
+.catch(err => console.log(err));
